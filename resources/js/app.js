3cdc19df91215376f8a048cc2798889487e4fb8a
Limon <limonkhan669@gmail.com>
2018-05-03 02:08:06 +0600

    SpeechRecognition put in anonymous function

--- resources/js/app.js
+++ resources/js/app.js
@@ -126,16 +126,13 @@ if (isChrome == true) {
     mic.style.display = "block";
 }
 
-mic.addEventListener('click', speech);
-
-function speech() {
+mic.addEventListener('click', function(){
     window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
     
       const recognition = new SpeechRecognition();
       recognition.interimResults = true;
     
       recognition.addEventListener('result', e =>{
-    
         const transcript = Array.from(e.results)
         .map(result => result[0])
         .map(result => result.transcript)
@@ -152,5 +149,9 @@ function speech() {
     //   recognition.addEventListener('end', recognition.start);
     
       recognition.start();
+});
+
+function speech() {
+    
 }
 
